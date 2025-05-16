96ee4943717d1e25596499793e48538aec8a973c
Du NV <du.nv@mor.com.vn>
2025-05-16 16:41:01 +0700

    add init plugins

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,13 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = {
+    "pyre",
+    "ansible-language-server",
+    "lua-language-server",
+    "bash-language-server",
+    "terraform-ls",
+    "yaml-language-server",
+}
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,28 +1,55 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+    {
+        "stevearc/conform.nvim",
+        event = "BufWritePre", -- uncomment for format on save
+        opts = require "configs.conform",
+    },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
-
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+        require "configs.lspconfig"
+        end,
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        lazy = false,
+        config = function()
+        require("nvim-tree").setup()
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "vim",
+                "lua",
+                "vimdoc",
+                "python",
+                "ansible",
+                "terraform",
+                "yaml",
+            },
+        },
+    },
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "lua-language-server",
+                "ansible-language-server",
+                "ansible-lint",
+                "stylua",
+                "prettier",
+                "pyre",
+                "bash-language-server",
+                "beautysh",
+                "shfmt",
+                "terraform-ls",
+                "yaml-language-server",
+                "yamlfmt",
+                "yamllint",
+            },
+        },
+    },
 }
